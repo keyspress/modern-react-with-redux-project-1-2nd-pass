892a725007ae4e0fa6564996f973fb3854567be9
Kyle Desktop <kylespresser@gmail.com>
2018-05-23 10:56:04 -0400

    lecture 26 add video list items and wire up youtube search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBdA_ygauQIXG7nQXBasUXdXWCFSd3ytrs';
 
-const API_KEY = 'AIzaSyBdA_ygauQIXG7nQXBasUXdXWCFSd3ytrs'
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos }); // key and value are the same, same as videos: videos 
+    });
+  }
+
+  render(){ 
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 
